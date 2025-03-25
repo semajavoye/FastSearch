3d144885afc8d5e2880068714cd8fa0088a191bc
Semaja Voye <semaja.voye5_8@gmx.de>
2025-03-25 21:28:32 +0100

    Add initial implementation of FastSearch with command line options and help message

---
+++ src/main.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[]) 
+{
+    // Get the passed arguments from the command line
+    if (argc > 1) 
+    {
+        for (int i = 1; i < argc; i++) 
+        {
+            if (string(argv[i]) == "-h" || string(argv[i]) == "--help") 
+            {
+                cout << "Welcome to FastSearch! Looks like you need help!" << std::endl;
+                cout << "Usage: fastsearch [options] [search term]" << std::endl;
+                cout << "Options:" << std::endl;
+                cout << "  -h, --help: Show this help message" << std::endl;
+                cout << "  -v, --version: Show the version of FastSearch" << std::endl;
+
+                return 0;
+            } 
+            else if (string(argv[i]) == "-v" || string(argv[i]) == "--version") 
+            {
+                cout << "FastSearch version 1.0.0" << std::endl;
+
+                return 0;
+            }
+        }
+    } else {
+        cout << "Welcome to FastSearch! Please provide a search term." << std::endl;
+        cout << "Usage: fastsearch [options] [search term]" << std::endl;
+        cout << "Use fastsearch -h for help." << std::endl;
+    }
+
+    return 0;
+}
