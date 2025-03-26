d7490b8353e05d47329f6c414fdcd6032faf05ae
Semaja Voye <semaja.voye5_8@gmx.de>
2025-03-26 19:18:59 +0100

    Refactor for better readability

--- src/main.cpp
+++ src/main.cpp
@@ -1,36 +1,57 @@
 #include <iostream>
+#include <string_view>
 #include "version.h"
 
-using namespace std;
+namespace fastsearch {
 
-int main(int argc, char *argv[]) 
-{
-    // Get the passed arguments from the command line
-    if (argc > 1) 
-    {
-        for (int i = 1; i < argc; i++) 
-        {
-            if (string(argv[i]) == "-h" || string(argv[i]) == "--help") 
-            {
-                cout << "Welcome to FastSearch! Looks like you need help!" << std::endl;
-                cout << "Usage: fastsearch [options] [search term]" << std::endl;
-                cout << "Options:" << std::endl;
-                cout << "  -h, --help: Show this help message" << std::endl;
-                cout << "  -v, --version: Show the version of FastSearch" << std::endl;
+constexpr std::string_view HelpMessage =
+    "Welcome to FastSearch! Looks like you need help!\n"
+    "Usage: fastsearch [options] [search term]\n"
+    "Options:\n"
+    "  -h, --help    Show this help message\n"
+    "  -v, --version Show the version of FastSearch\n";
 
-                return 0;
-            } 
-            else if (string(argv[i]) == "-v" || string(argv[i]) == "--version") 
-            {
-                cout << "FastSearch version " << FASTSEARCH_VERSION << endl;
-                return 0;
-            }
-        }
-    } else {
-        cout << "Welcome to FastSearch! Please provide a search term." << std::endl;
-        cout << "Usage: fastsearch [options] [search term]" << std::endl;
-        cout << "Use fastsearch -h for help." << std::endl;
+constexpr std::string_view UsageMessage =
+    "Welcome to FastSearch! Please provide a search term.\n"
+    "Usage: fastsearch [options] [search term]\n"
+    "Use fastsearch -h for help.\n";
+
+void PrintVersion() {
+    std::cout << "FastSearch version " << FASTSEARCH_VERSION << std::endl;
+}
+
+void PrintHelp() {
+    std::cout << HelpMessage;
+}
+
+void PrintUsage() {
+    std::cout << UsageMessage;
+}
+
+void ProcessArguments(int argc, char* argv[]) {
+    if (argc <= 1) {
+        PrintUsage();
+        return;
     }
 
+    for (int i = 1; i < argc; ++i) {
+        std::string_view arg = argv[i];
+
+        if (arg == "-h" || arg == "--help") {
+            PrintHelp();
+            return;
+        }
+        
+        if (arg == "-v" || arg == "--version") {
+            PrintVersion();
+            return;
+        }
+    }
+}
+
+}  // namespace fastsearch
+
+int main(int argc, char* argv[]) {
+    fastsearch::ProcessArguments(argc, argv);
     return 0;
 }
